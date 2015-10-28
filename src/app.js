7753318c3350c657ae98da06c14845a191c65878
lbendlin <lutz@bendlin.us>
2015-10-27 23:09:51 -0400

    refactoring
    cleanup of function calls, implemented home and away logic

--- src/app.js
+++ src/app.js
@@ -4,11 +4,14 @@ var ajax = require('ajax');
 //var Accel = require('ui/accel');
 //var Vibe = require('ui/vibe');
 
+var resultsMenu ;
 var menuItems = [];
 var statData = [];
 var baseURL = 'http://lbendlin.dyndns.info:8081/t/';
 var selected = 0;
 var statusLine = '';
+var req = '';
+var status = '';
 
 // Create a splashscreen with title and subtitle
 var splashCard = new UI.Card({
@@ -43,69 +46,147 @@ detailCard.on('click', 'down', function() {
     detailCard.body('Target:' + statData.thermostats[selected].t_heat + '\n' + statusLine + '\nOutside:' + statData.outside);
 });
 detailCard.on('click', 'select', function() {
-    // ajax call to set
-    var req = 'postjson.aspx?URL=http://' + statData.thermostats[selected].ip;
-    req += '/tstat&json={"tmode":1,"t_heat":' + statData.thermostats[selected].t_heat;
-    req += ',"hold":' + statData.thermostats[selected].hold + '}' + '&r=' + Math.random();
+  setTherm(selected) ; 
+});
+// toggle hold
+detailCard.on('longClick', 'select', function() {
+  statData.thermostats[selected].hold = 1 - statData.thermostats[selected].hold;
+  setTherm(selected) ;   
+});
+
+function getStatus(k) {
+  //console.log('Getting status for '+statData.thermostats[k].name);
+        req = 'getjson.aspx?URL=http://' + statData.thermostats[k].ip + '/tstat&r=' + Math.random();
+          ajax({url: baseURL + req, type: 'json' },
+          function(data) {
+             statData.thermostats[k].t_heat=data.t_heat;
+             statData.thermostats[k].tstate=data.tstate;
+             statData.thermostats[k].hold=data.hold;
+             statData.thermostats[k].override=data.override;
+             status = statData.thermostats[k].name + ' ' + statData.thermostats[k].temp + '/' + statData.thermostats[k].t_heat;
+             status += statData.thermostats[k].tstate  ? ' (on)' : '';
+            resultsMenu.item(0, k, { title: status });
+            resultsMenu.section(0, { title: 'Got ' + statData.thermostats[k].name});
+            
+            //console.log('success getting Status for '+statData.thermostats[k].name);
+           },
+           function(error) {
+            console.log('failed to get Status for '+statData.thermostats[k].name);
+           }
+        );        
+}
+
+function getProgram(m) {
+  //console.log('Getting status for '+statData.thermostats[m].name);
+        req = 'getjson.aspx?URL=http://' + statData.thermostats[m].ip + '/tstat/program/heat&r=' + Math.random();
+          ajax({url: baseURL + req, type: 'json' },
+          function(data) {
+            //resultsMenu.section(0, { title: 'Set home '});
+            getStatus(m);
+           },
+           function(error) {
+            console.log('failed to get program for '+statData.thermostats[m].name);
+           }
+        );        
+}
+
+function setHome(n) {
+      req = 'postjson.aspx?URL=http://' + statData.thermostats[n].ip;
+    req += '/tstat&json={"tmode":1,"hold":0}&r=' + Math.random();
 
     ajax({url: baseURL + req, type: 'text' },
         function(data) {
-            //did it work?
-            if (data.indexOf("success") != -1) {
-              detailCard.body('Set to '+statData.thermostats[selected].t_heat);
-              // ajax call to get new status
-              req = 'getjson.aspx?URL=http://' + statData.thermostats[selected].ip + '/tstat&r=' + Math.random();
-              ajax({url: baseURL + req, type: 'json' },
-                   function(data) {
-                     statData.thermostats[selected].t_heat=data.t_heat;
-                     statData.thermostats[selected].tstate=data.tstate;
-                     statData.thermostats[selected].hold=data.hold;
-                     statData.thermostats[selected].override=data.override;
-                     detailCard.hide();
-                   },
-                   function(error) {
-                      detailCard.body(error);
-                   }
-               );    
-            } else {
-                detailCard.body(data);
-            }
+            getProgram(n);
         },
         function(error) {
-            detailCard.body(error);
+            console.log('failed to set Home for '+statData.thermostats[n].name);
         }
     );
-});
+}
 
-var resultsMenu = new UI.Menu({
+function setAway(n) {
+      req = 'postjson.aspx?URL=http://' + statData.thermostats[n].ip;
+    req += '/tstat&json={"tmode":1,"t_heat":61,"hold":1}&r=' + Math.random();
+
+    ajax({url: baseURL + req, type: 'text' },
+        function(data) {
+            getStatus(n);
+        },
+        function(error) {
+            console.log('failed to set Home for '+statData.thermostats[n].name);
+        }
+    );
+}
+
+function setTherm(i) {
+      req = 'postjson.aspx?URL=http://' + statData.thermostats[i].ip;
+    req += '/tstat&json={"tmode":1,"t_heat":' + statData.thermostats[i].t_heat;
+  req +=',"hold":' + statData.thermostats[i].hold + '}&r=' + Math.random();
+
+    ajax({url: baseURL + req, type: 'text' },
+        function(data) {
+          detailCard.body('Set to '+statData.thermostats[i].t_heat);  
+          setTimeout(function () { detailCard.hide(); },1000);
+        },
+        function(error) {
+            detailCard.body('failed to set temp to '+statData.thermostats[i].t_heat);
+        }
+    );
+}
+
+resultsMenu = new UI.Menu({
     sections: [{
+      title: 'Thermostats',
         items: menuItems
     }]
+});
+// refresh data from actual stats. all or just the one we touched?
+resultsMenu.on('show', function() {
+    //for (var j = 0; j < statData.count; j++) {
+        // ajax call to get new status, update menu
+    //  getStatus(j);
+    //}
+  getStatus(selected);
+  
 });
 resultsMenu.on('select', function(e) {
     selected = e.itemIndex;
     detailCard.show();
 });
+resultsMenu.on('longSelect', function(e) {
+    // toggle home/away for all
+  if(( statData.thermostats[0].t_heat == 61) && statData.thermostats[0].hold ) {
+    resultsMenu.section(0, { title: 'Setting Home'});
+  for (var i = 0; i < statData.count; i++) {
+    setHome(i);
+  }    
+  } else {
+    resultsMenu.section(0, { title: 'Setting Away'});
+   for (var j = 0; j < statData.count; j++) {
+    setAway(j);
+  }       
+  }
+
+});
 
 // Display the splash
 splashCard.show();
 
-//get list of thermostats and their current data from database
+//get initial list of thermostats and their current data from database
 ajax({url: baseURL + 'tall.asp', type: 'json'},
     function(data) {
         statData = data;
         // load menu items
         for (var i = 0; i < statData.count; i++) {
+            var status = statData.thermostats[i].name + ' ' + statData.thermostats[i].temp + '/' + statData.thermostats[i].t_heat;
+            status += statData.thermostats[i].tstate  ? ' (on)' : '';
             menuItems.push({
-                title: statData.thermostats[i].name + '  ' + statData.thermostats[i].temp
+                title: status
             });
         }
-        // Construct Menu to show to user
-
         // Show the Menu, hide the splash
         resultsMenu.show();
         splashCard.hide();
-
     },
     function(error) {
         // Failure!
