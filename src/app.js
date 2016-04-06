c45866379ebc22ac661da0a2c2f38d920f7c2248
lbendlin <lutz@bendlin.us>
2016-04-06 17:29:22 -0400

    Garage door opening first

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
 var UI = require('ui');
 var ajax = require('ajax');
-//var Vector2 = require('vector2');
 var Accel = require('ui/accel');
 var Vibe = require('ui/vibe');
 
+var version = 'v1.07 20160406';
+var baseURL = 'http://lbendlin.dyndns.info:8081/t/';
 var resultsMenu ;
 var menuItems = [];
+var forecastItems = [];
 var statData = [];
-var baseURL = 'http://lbendlin.dyndns.info:8081/t/';
 var selected = 0;
 var statusLine;
 var req;
 var timeOut;
 var firstTime = true;
+var hasForecast = false;
 var doorClosed = 1;
 
 // Create a splashscreen with title and subtitle
@@ -23,11 +25,13 @@ var splashCard = new UI.Card({
 
 var doorCard = new UI.Card({
     action: {
-        select: 'images/confirm.png'
+        //up: 'images/plus.png',
+        select: 'IMAGES_CONFIRM_PNG'//,
+        //down: 'images/minus.png'
     }
 });
 doorCard.on('show', function() {
-  clearTimeout(timeOut);
+  //clearTimeout(timeOut);
   statusLine = 'Door is ';
   statusLine += doorClosed ? 'closed.' : 'open.';
   doorCard.title(statusLine);
@@ -37,6 +41,7 @@ doorCard.on('show', function() {
   timeOut = setTimeout(function () { doorCard.hide(); },60000);
 });
 doorCard.on('click', 'select', function() {
+  //clearTimeout(timeOut);
   statusLine = doorClosed ? "Open" : "Clos";
   statusLine += 'ing door...';
   doorCard.title(statusLine);  
@@ -52,18 +57,19 @@ doorCard.on('click', 'select', function() {
         function(error) {
             doorCard.body('failed to operate door');
         }
-    );   
+    ); 
+  timeOut = setTimeout(function () { doorCard.hide(); },60000);
 });
 
 var detailCard = new UI.Card({
     action: {
-        up: 'images/plus.png',
-        select: 'images/confirm.png',
-        down: 'images/minus.png'
+        up: 'IMAGES_PLUS_PNG',
+        select: 'IMAGES_CONFIRM_PNG',
+        down: 'IMAGES_MINUS_PNG'
     }
 });
 detailCard.on('show', function() {
-  clearTimeout(timeOut);
+  //clearTimeout(timeOut);
     statusLine = statData.thermostats[selected].tstate ? 'On ' : '';
     statusLine += statData.thermostats[selected].override ? 'Over ' : '';
     statusLine += statData.thermostats[selected].hold ? 'Hold' : '';
@@ -75,19 +81,24 @@ detailCard.on('show', function() {
 });
 detailCard.on('click', 'up', function() {
     // Up click detected!
+  //clearTimeout(timeOut);
     statData.thermostats[selected].t_heat++;
     detailCard.subtitle('New target ' + statData.thermostats[selected].t_heat);
+  timeOut = setTimeout(function () { detailCard.hide(); },60000);
 });
 detailCard.on('click', 'down', function() {
     // Down click detected!
+  //clearTimeout(timeOut);
     statData.thermostats[selected].t_heat--;
     detailCard.subtitle('New target ' + statData.thermostats[selected].t_heat);
+  timeOut = setTimeout(function () { detailCard.hide(); },60000);
 });
 detailCard.on('click', 'select', function() {
   setTherm(selected) ; 
 });
 // toggle hold
 detailCard.on('longClick', 'select', function() {
+  Vibe.vibrate('short');
   statData.thermostats[selected].hold = 1 - statData.thermostats[selected].hold;
   setTherm(selected) ;   
 });
@@ -105,10 +116,10 @@ function getStatus(i) {
             statData.thermostats[i].tstate = data.tstate;
             statData.thermostats[i].hold = data.hold;
             statData.thermostats[i].override = data.override;
-            resultsMenu.item(0, i, {
+            resultsMenu.item(1, i, {
                 subtitle: status(i)
             });
-            resultsMenu.section(0, {
+            resultsMenu.section(1, {
                 title: 'Got ' + statData.thermostats[i].name
             });
         },
@@ -123,7 +134,7 @@ function getProgram(i) {
         req = 'getjson.aspx?URL=http://' + statData.thermostats[i].ip + '/tstat/program/heat&r=' + Math.random();
           ajax({url: baseURL + req, type: 'json' },
           function(data) {
-            //resultsMenu.section(0, { title: 'Set home '});
+            //resultsMenu.section(1, { title: 'Set home '});
             getStatus(i);
            },
            function(error) {
@@ -184,7 +195,7 @@ function getDoorStatus(i) {
         },
         function(data) {
           doorClosed = data.doorClosed;  
-            resultsMenu.item(1, 0, {
+            resultsMenu.item(0, 0, {
                 subtitle: doorClosed == 1 ? 'closed' : 'open'
             });
         },
@@ -202,18 +213,54 @@ function status(i) {
   return subTitle;
 }
 
+function getForecast() {
+  //get weather data
+ ajax({url: 'http://api.wunderground.com/api/404d7aebd67c26ec/hourly/q/01721.json', type: 'json'},
+    function(data) {
+        // load weather forecast items
+      hasForecast = true;
+      forecastItems = [];  
+      for (var i = 0; i < 20; i++) {
+            forecastItems.push({
+                title: data.hourly_forecast[i].FCTTIME.civil + ' ' + data.hourly_forecast[i].temp.english + ' F',
+              subtitle: data.hourly_forecast[i].condition == data.hourly_forecast[i].wx ? data.hourly_forecast[i].condition : data.hourly_forecast[i].wx + ', ' + data.hourly_forecast[i].condition
+            });
+        }
+        // Show the forecast in the Menu
+        resultsMenu.section(2, {
+            title: 'Forecast ',
+            items: forecastItems
+        });
+    },
+    function(error) {
+        // Failure!
+      hasForecast = false;
+      resultsMenu.section(2, {
+            title: 'Forecast ',
+            items: [{
+                title: 'Error' ,
+          subtitle:  'Cannot get Forecast'
+            }]
+        });
+    }
+);
+}
+
 resultsMenu = new UI.Menu({
     sections: [{
-      title: 'Thermostats',
-        items: menuItems
-    },{
       title: 'Garage',
         items: [{
                 title: 'Garage door is' ,
           subtitle: doorClosed == 1 ? 'closed' : 'open'
             }]
     },{
-      title: 'v1.02 20151203',
+      title: 'Thermostats',
+        items: menuItems
+    },{
+      title: 'Current Outside',
+        items: forecastItems
+    },{
+      title: version,
     }]
 });
 // refresh data from actual stats. all on first call, then just the one we touched
@@ -227,37 +274,74 @@ resultsMenu.on('show', function() {
     getDoorStatus();
   } else {
     getStatus(selected);
-    resultsMenu.item(1, 0, {
+    resultsMenu.item(0, 0, {
                 subtitle: doorClosed == 1 ? 'closed' : 'open'
     });  
-    clearTimeout(timeOut);
+    //clearTimeout(timeOut);
   }
   // exit after 1 minute
   timeOut = setTimeout(function () { resultsMenu.hide(); },60000);
 });
 resultsMenu.on('select', function(e) {
-  if(e.sectionIndex == 0) {
-    selected = e.itemIndex;
-    detailCard.show();
-  } else {
-    doorCard.show();
+  switch(e.sectionIndex) {
+    case 0:
+      doorCard.show();
+      break;
+    case 1:
+      selected = e.itemIndex;
+      detailCard.show();
+      break;
+    case 2:
+      //get forecast ?
+      if (hasForecast)
+        break;
+      resultsMenu.section(2, {
+            title: 'Getting Forecast ...'
+        });
+      getForecast();
+      break;
   }
 });
 resultsMenu.on('longSelect', function(e) {
-    // toggle home/away for all
+  // operate garage door without confirmation  
+ 
+  Vibe.vibrate('short');
+    timeOut = setTimeout(function () { resultsMenu.hide(); },60000);
+ switch(e.sectionIndex) {
+    case 0:
+     resultsMenu.section(0, { title: 'Operating door'});
+     req = 'setdooractionpebble.aspx';
+     ajax({url: baseURL + req, type: 'text' },
+        function(data) {
+          Vibe.vibrate('short');
+          doorClosed = data.indexOf('closed') != -1 ? 1 : 0;
+          resultsMenu.section(0, { title: 'Garage door is'});
+          resultsMenu.item(0, 0, {subtitle: doorClosed == 1 ? 'closed' : 'open' });  
+         },
+        function(error) {
+          resultsMenu.item(0, 0, { subtitle: 'Failed to operate door'});  
+         
+        }
+          
+    ); 
+           break;
+      // toggle home/away for all thermostats
+    case 1: 
+
   var i;
   if(( statData.thermostats[0].t_heat == 61) && statData.thermostats[0].hold ) {
-    resultsMenu.section(0, { title: 'Setting Home'});
+    resultsMenu.section(1, { title: 'Setting Home'});
   for (i = 0; i < statData.count; i++) {
     setHome(i);
   }    
   } else {
-    resultsMenu.section(0, { title: 'Setting Away'});
+    resultsMenu.section(1, { title: 'Setting Away'});
    for (i = 0; i < statData.count; i++) {
     setAway(i);
   }       
   }
-
+  break;
+ }
 });
 
 // Display the splash
@@ -274,6 +358,9 @@ ajax({url: baseURL + 'tall.asp', type: 'json'},
               subtitle: status(i)
             });
         }
+        forecastItems.push({
+          title: 'Temp: ' + statData.outside + 'F'
+        });
         // Show the Menu, hide the splash
         resultsMenu.show();
         splashCard.hide();
@@ -286,9 +373,8 @@ ajax({url: baseURL + 'tall.asp', type: 'json'},
 
 // Prepare the accelerometer
 Accel.init();
+// show garage screen when shaking watch
 Accel.on('tap', function(e) {
-  console.log('Tap event on axis: ' + e.axis + ' and direction: ' + e.direction);
+  //console.log('Tap event on axis: ' + e.axis + ' and direction: ' + e.direction);
   doorCard.show();
 });
-// Notify the user
-//Vibe.vibrate('short');
